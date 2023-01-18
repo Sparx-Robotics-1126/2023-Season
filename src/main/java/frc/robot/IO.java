7c4f414b25f84f58dd62e490426ab7fae0814097
FlareWare95 <100642537+FlareWare95@users.noreply.github.com>
2023-01-17 21:21:13 -0500

    Update IO.java

--- src/main/java/frc/robot/IO.java
+++ src/main/java/frc/robot/IO.java
@@ -16,15 +16,9 @@ public final class IO
 	public static final int DRIVES_LEFT_MOTOR_1				= 27;
 	public static final int DRIVES_LEFT_MOTOR_2				= 25;
 
-	public static final int ACQUISITIONS_ARM_MOTOR			= 3;
-	public static final int ACQUISITIONS_INTAKE_MOTOR		= 11;
-
-	public static final int SHOOTER_MOTOR					= 1;
-	public static final int STORAGE_MOTOR					= 2;
 	
 	//SENSORS
 	public static final I2C.Port I2C_ONBOARD				= I2C.Port.kOnboard;
 	public static final SerialPort.Port USB_ONBOARD 		= SerialPort.Port.kUSB;
 
-	public static final int STORAGE_IR						= 18;
 }
