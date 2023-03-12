2cd4acc8c06124ec434c7ccf4d77a35cf371a163
Keith <kkarnisky@ivedix.com>
2023-03-11 22:00:07 -0500

    fixwd build error

--- src/main/java/frc/robot/commands/TurnRight.java
+++ src/main/java/frc/robot/commands/TurnRight.java
@@ -1,30 +1,46 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystem.DriveSubsystem;
 
 public class TurnRight extends CommandBase{
-
+   
+	
+	private double minSpeed = 0.3;
+	private double finalAngle = 0;
+	private double stopAngle = 90;
+	private double speedToStop = 0;
+	private double distanceToStop = 0;
    
     DriveSubsystem m_drive;
     double m_turnAngle;
 double m_finalAngle;
 
+    /**
+     * @param drive
+     * @param turnAngle
+     */
     public TurnRight(DriveSubsystem drive, double turnAngle) {
         m_drive = drive;
         m_turnAngle = turnAngle;
+       
 
-        m_finalAngle = m_drive.getRotation + turnAngle;
-
+        m_finalAngle = m_drive.getRotation() + turnAngle;
+        minSpeed = 0.3;
+        finalAngle = 0;
+        stopAngle = 90;
+         speedToStop = 0;
+       distanceToStop = 0;
 
         addRequirements(drive);
     }
 
     @Override
     public void execute() {
-        distanceToStop = Math.abs(m_finalAngle - m_drive.getRotation);
+        distanceToStop = Math.abs(m_finalAngle - m_drive.getRotation());
 
-		if (m_drive.getRotation >= m_finalAngle)
-			return m_drive.tankDrive(0,0);
+		if (m_drive.getRotation() >= m_finalAngle)
+			 m_drive.tankDrive(0,0);
 		else if (distanceToStop <= stopAngle)
 		{
 			speedToStop = distanceToStop / stopAngle;
@@ -33,9 +49,9 @@ double m_finalAngle;
 			if (speedToStop < minSpeed)
 				speedToStop = minSpeed;
 
-			return m_drive.tankDrive(speedToStop,-speedToStop) ;
+			 m_drive.tankDrive(speedToStop,-speedToStop) ;
 		}
 			
-		return new DrivesOutput(SPEED, -SPEED);
+        m_drive.tankDrive(.3, -.3);
     }
 }
