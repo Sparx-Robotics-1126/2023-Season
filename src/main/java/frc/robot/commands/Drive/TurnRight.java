e88c792e5703b8d735e1191fc1186d61cb99929a
Keith <kkarnisky@ivedix.com>
2023-03-14 12:13:26 -0400

    cleaned warnings

--- src/main/java/frc/robot/commands/Drive/TurnRight.java
+++ src/main/java/frc/robot/commands/Drive/TurnRight.java
@@ -4,38 +4,36 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystem.DriveSubsystem;
 
-public class TurnRight extends CommandBase{
-   
-	
-	private double minSpeed = 0.3;
-	private double finalAngle = 0;
-	private double stopAngle = 90;
-	private double speedToStop = 0;
-	private double distanceToStop = 0;
-    private double startAngle = 0;
+public class TurnRight extends CommandBase {
+
+    private double minSpeed = 0.3;
+    // private double finalAngle = 0;
+    // private double stopAngle = 90;
+    private double speedToStop = 0;
+    private double distanceToStop = 0;
+    // private double startAngle = 0;
     private double speed = .6;
-   
+
     DriveSubsystem m_drive;
     double m_turnAngle;
-double m_finalAngle;
+    double m_finalAngle;
 
     /**
      * @param drive
      * @param turnAngle
      */
-    public TurnRight( double turnAngle,DriveSubsystem drive) {
+    public TurnRight(double turnAngle, DriveSubsystem drive) {
         m_drive = drive;
         m_turnAngle = turnAngle;
-       
-        startAngle = m_drive.getHeading();
+
+        // startAngle = m_drive.getHeading();
         m_finalAngle = m_drive.getHeading() + turnAngle;
         SmartDashboard.putNumber("FINAL_ANGLE", m_finalAngle);
         minSpeed = 0.35;
-        finalAngle = 0;
-        stopAngle = 90;
-         speedToStop = 0;
-       distanceToStop = 0;
-
+        // finalAngle = 0;
+        // stopAngle = 90;
+        speedToStop = 0;
+        distanceToStop = 0;
 
         addRequirements(drive);
     }
@@ -48,7 +46,7 @@ double m_finalAngle;
 
     @Override
     public void execute() {
-			
+
         m_drive.tankDrive(-speed, speed);
     }
 
@@ -56,30 +54,27 @@ double m_finalAngle;
     public boolean isFinished() {
         distanceToStop = Math.abs(m_finalAngle - m_drive.getHeading());
         SmartDashboard.putNumber("DISTANCE_TO_STOP", distanceToStop);
-                
-                if (m_drive.getHeading() >= m_finalAngle){
-                   return true;
-                }
-                else if (distanceToStop <= m_finalAngle)
-                {
-                    speedToStop = distanceToStop / m_finalAngle;
-        
-                    //Slow down the robot when we near the desired angle to avoid overshooting it.
-                    if (speedToStop < minSpeed)
-                        speedToStop = minSpeed;
-        
-                     m_drive.tankDrive(-speedToStop,speedToStop) ;
-                     return false;
-                }
-                    
-                m_drive.tankDrive(-speed, speed);
-                return false;
+
+        if (m_drive.getHeading() >= m_finalAngle) {
+            return true;
+        } else if (distanceToStop <= m_finalAngle) {
+            speedToStop = distanceToStop / m_finalAngle;
+
+            // Slow down the robot when we near the desired angle to avoid overshooting it.
+            if (speedToStop < minSpeed)
+                speedToStop = minSpeed;
+
+            m_drive.tankDrive(-speedToStop, speedToStop);
+            return false;
+        }
+
+        m_drive.tankDrive(-speed, speed);
+        return false;
     }
 
     @Override
     public void end(boolean interrupted) {
-        m_drive.tankDrive(0,0);
+        m_drive.tankDrive(0, 0);
     }
 
-   
 }
--- src/main/java/frc/robot/commands/Drive/TurnToRelativeAngle.java
+++ src/main/java/frc/robot/commands/Drive/TurnToRelativeAngle.java
@@ -7,50 +7,51 @@ import frc.robot.Constants.DriveConstants;
 import frc.robot.subsystem.DriveSubsystem;
 
 public class TurnToRelativeAngle extends PIDCommand {
-    private final DriveSubsystem drive;
-  
-   
-    private final PIDController controller;
-    /**
-     * Turns to robot to the specified angle.
-     *
-     * @param targetAngleDegrees The angle to turn to
-     * @param drive              The drive subsystem to use
-     */
-    public TurnToRelativeAngle(double targetRelativeAngleDegrees, DriveSubsystem drive) {
-      super(
-          new PIDController(DriveConstants.kRelTurnP, DriveConstants.kRelTurnI, DriveConstants.kRelTurnD),
-          // Close loop on heading
-          drive::getRotation,
-          // Set reference to target
-          targetRelativeAngleDegrees,
-          // Pipe output to turn robot
+  // private final DriveSubsystem drive;
+
+  private final PIDController controller;
+
+  /**
+   * Turns to robot to the specified angle.
+   *
+   * @param targetAngleDegrees The angle to turn to
+   * @param drive              The drive subsystem to use
+   */
+  public TurnToRelativeAngle(double targetRelativeAngleDegrees, DriveSubsystem drive) {
+    super(
+        new PIDController(DriveConstants.kRelTurnP, DriveConstants.kRelTurnI, DriveConstants.kRelTurnD),
+        // Close loop on heading
+        drive::getRotation,
+        // Set reference to target
+        targetRelativeAngleDegrees,
+        // Pipe output to turn robot
         output -> {
-                drive.arcadeDrive(0, output);
-                SmartDashboard.putNumber("OUTPUT", output);
+          drive.arcadeDrive(0, output);
+          SmartDashboard.putNumber("OUTPUT", output);
         },
-          // Require the drive
-          drive);
-      this.drive = drive;
-      this.controller = getController();
-      // Set the controller to be continuous (because it is an angle controller)
-      controller.enableContinuousInput(-180, 180);
-      // Set the controller tolerance - the delta tolerance ensures the robot is stationary at the
-      // setpoint before it is considered as having reached the reference
-      controller.setTolerance(DriveConstants.kRelTurnToleranceDeg, DriveConstants.kRelTurnRateToleranceDegPerS);
-    }
-  
-    @Override
-    public boolean isFinished() {
-      // End when the controller is at the reference.
-      return controller.atSetpoint();
-    }
-  
-  //   public double getPositionError() {
-  //     return controller.getPositionError();
-  //   }
-  
-  //   public double getVelocityError() {
-  //     return controller.getVelocityError();
-  //   }
-   }
+        // Require the drive
+        drive);
+    // this.drive = drive;
+    this.controller = getController();
+    // Set the controller to be continuous (because it is an angle controller)
+    controller.enableContinuousInput(-180, 180);
+    // Set the controller tolerance - the delta tolerance ensures the robot is
+    // stationary at the
+    // setpoint before it is considered as having reached the reference
+    controller.setTolerance(DriveConstants.kRelTurnToleranceDeg, DriveConstants.kRelTurnRateToleranceDegPerS);
+  }
+
+  @Override
+  public boolean isFinished() {
+    // End when the controller is at the reference.
+    return controller.atSetpoint();
+  }
+
+  // public double getPositionError() {
+  // return controller.getPositionError();
+  // }
+
+  // public double getVelocityError() {
+  // return controller.getVelocityError();
+  // }
+}
