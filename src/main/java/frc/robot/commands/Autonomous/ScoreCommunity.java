d59171af3f9e13f4e7ab1df5a3b3f02a5783e5c1
Keith <kkarnisky@ivedix.com>
2023-03-16 10:37:41 -0400

    auto added drop cone low score

--- src/main/java/frc/robot/commands/Autonomous/ScoreCommunity.java
+++ src/main/java/frc/robot/commands/Autonomous/ScoreCommunity.java
@@ -3,6 +3,7 @@ package frc.robot.commands.Autonomous;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.subsystem.DriveSubsystem;
+import frc.robot.commands.Acquisition.MoveTo;
 import frc.robot.commands.Drive.DriveDistance;
 import frc.robot.subsystem.AcquisitionSubsystem;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
@@ -13,10 +14,13 @@ public class ScoreCommunity extends SequentialCommandGroup {
     
     //moving forward to score cone
     addCommands(new DriveDistance(drive, .5, .7).withTimeout(5));
-    addCommands(new WaitCommand(1));
+    addCommands(new MoveTo(0, .2, acquisitions));
+    addCommands(new InstantCommand(() -> acquisitions.grabberOpen()));
+    
+    //addCommands(new WaitCommand(1));
     //release cone
     addCommands((new InstantCommand( () -> acquisitions.grabberOpen())));
-    addCommands(new WaitCommand(1));
+    addCommands(new WaitCommand(.5));
     //moving backward to leave commmunity
     addCommands(new DriveDistance(drive, -3, .7).withTimeout(10));
 
